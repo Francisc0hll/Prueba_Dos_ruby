9ee65a4736294e9153b4ecf09eb488d39a805c4f
Francisco Javier Hernandez LLewellyn <j.francisco.hll@gmail.com>
2019-05-14 11:03:46 -0400

    primer commit, copia de metodo de lectura

---
+++ prueba_dos_ruby.rb
@@ -0,0 +1,7 @@
+#PRUEBA DOS en RUBY (desafio LATAM)
+def read_alum(file_name)
+    file=File.open(file_name, 'r')
+    alum=file.readlines.map(&:chomp).map { |lines| lines.split(', ') }
+    file.close
+    alum
+end
