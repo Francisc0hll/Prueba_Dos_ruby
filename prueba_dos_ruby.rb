52c99d5a8cfe0a3b440d62e8b74f009969b9e40f
Francisco Javier Hernandez LLewellyn <j.francisco.hll@gmail.com>
2019-05-14 12:02:14 -0400

    se implementa metodo que muestra los alumnos aprobados con una nota dada, si no es asi el metodo muestra los alumnos aprobados con nota mayor a 5.0

--- prueba_dos_ruby.rb
+++ prueba_dos_ruby.rb
@@ -8,7 +8,9 @@ end
 
 def menu
     
-    puts "Bienvenido al sistema de entrega de notas de curso G22 \n"
+    
+    puts "\nBienvenido al sistema de entrega de notas de curso G22 \n"
+    puts "########################################################\n"
     puts "ingrese una de las siguientes opciones del menú \n"
     puts "1) Listar todos los alumnos y sus promedios: \n"
     puts "2) listar todos los alumnos y su cantidad de inasistencias : \n"
@@ -33,8 +35,23 @@ def cantidad_inasistencias(arreglo)
         elemento.each_with_index do |elem, indice|
             contador_inasistencias += 1 if elem == 'A' && indice != 0
         end
-        puts "Alumno : #{elemento[0]}, tiene : #{contador_inasistencias} inasistencias"
+        puts "Alumno : #{elemento[0]}, tiene : #{contador_inasistencias} inasistencias \n"
     end
+end
+
+def alumnos_aprobados(arreglo, nota_aprobacion = 5.0)
+    arreglo.each_with_index do |elemento, index|
+        promedio = 0
+        suma = 0
+        elemento.each_with_index do |elem, indice|
+            suma += elem.to_i if indice != 0
+        end
+        promedio = suma.to_f/(elemento.length-1)
+        if promedio >= nota_aprobacion
+            puts "Alumno : #{elemento[0]} Aprobo con nota : #{promedio}\n"
+        end
+    end
+
 end
 
 arreglo_alumnos = read_alum('alumnos.csv')
@@ -48,14 +65,31 @@ while opcion != 4
     case opcion
         
     when 1
+
         promedio_alumnos(arreglo_alumnos)
+
     when 2
+
         cantidad_inasistencias(arreglo_alumnos)
+
     when 3
+
+        aux_nota = 0
+        puts "\n \t ingrese nota de aprobacion, si no es ingresada por defecto sera un 5.0 \n"
+        aux_nota = gets.chomp.to_f
+        if aux_nota != 0.0
+            alumnos_aprobados(arreglo_alumnos,aux_nota)
+        else
+            alumnos_aprobados(arreglo_alumnos)
+        end
         
     when 4
+
         puts "salio de sistema de notas de curso.\n Adios."
+
     else
+
         puts "opcion invalida, ingrese una opcion que corresponda: \n"
+
     end
 end
