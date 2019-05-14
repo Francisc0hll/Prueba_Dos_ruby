2b5689419579928c945901f658c804d519f3b1a0
Francisco Javier Hernandez LLewellyn <j.francisco.hll@gmail.com>
2019-05-14 11:13:35 -0400

    se ingresa el menu a desplegar

--- prueba_dos_ruby.rb
+++ prueba_dos_ruby.rb
@@ -5,3 +5,14 @@ def read_alum(file_name)
     file.close
     alum
 end
+
+def menu
+    
+    puts "Bienvenido al sistema de entrega de notas de curso G22 \n"
+    puts "ingrese una de las siguientes opciones del menú \n"
+    puts "1) Listar todos los alumnos y sus promedios: \n"
+    puts "2) listar todos los alumnos y su cantidad de inasistencias : \n"
+    puts "3) Listar los alumnos que aprobaron el curso, dando la posibilidad de ingresar la nota de aprobacion ( si no es entegada, esta sera 5.0 ): \n"
+    puts "4) Salir del sistema de entrega de notas."
+
+end
