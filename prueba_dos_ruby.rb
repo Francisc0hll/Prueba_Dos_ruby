8b0a7695e1c39fdc674d9eb35e054bd7be2a3bee
Francisco Javier Hernandez LLewellyn <j.francisco.hll@gmail.com>
2019-05-14 11:35:23 -0400

    ingresa metodo para sacar primedios e imprimirlos en pantalla despues de ingresar opcion 1 del menu

--- prueba_dos_ruby.rb
+++ prueba_dos_ruby.rb
@@ -14,5 +14,38 @@ def menu
     puts "2) listar todos los alumnos y su cantidad de inasistencias : \n"
     puts "3) Listar los alumnos que aprobaron el curso, dando la posibilidad de ingresar la nota de aprobacion ( si no es entegada, esta sera 5.0 ): \n"
     puts "4) Salir del sistema de entrega de notas."
+    
+end
 
+def promedio_alumnos(arreglo)
+    arreglo.each_with_index do |elemento, index|
+        suma = 0
+        elemento.each_with_index do |elem, indice|
+            suma += elem.to_i if indice != 0
+        end
+        puts "Alumno : #{elemento[0]}, Promedio : #{suma.to_f/(elemento.length-1)} \n"
+    end
+end
+
+arreglo_alumnos = read_alum('alumnos.csv')
+
+opcion = 0
+
+while opcion != 4
+    menu
+    opcion = gets.chomp.to_i
+    
+    case opcion
+        
+    when 1
+        promedio_alumnos(arreglo_alumnos)
+    when 2
+        
+    when 3
+        
+    when 4
+        puts "salio de sistema de notas de curso.\n Adios."
+    else
+        puts "opcion invalida, ingrese una opcion que corresponda: \n"
+    end
 end
